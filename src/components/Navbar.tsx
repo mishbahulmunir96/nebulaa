3b887b6aa7c0b27c2f4cd8d0cef4da8643905630
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-11-11 22:59:19 +0700

    add animation scroll navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Logo from "./elements/Logo";
 import MenuBar from "./fragments/MenuBar";
 import SearchBar from "./fragments/SearchBar";
 
 const Navbar = () => {
+  const [isBlured, setIsBlured] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsBlured(true);
+      } else {
+        setIsBlured(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="fixed z-50 flex h-20 w-full items-center justify-center px-2 lg:px-36">
+    <header
+      className={`fixed z-50 flex h-20 w-full items-center justify-center px-2 transition-all lg:px-36 ${isBlured ? "bg-yellow-200/70 backdrop-blur-md" : "bg-transparent"}`}
+    >
       <nav className="container mx-auto flex items-center justify-between">
         <Logo classname="order-2 lg:order-none"></Logo>
         <MenuBar classname="order-1 lg:order-none" />
