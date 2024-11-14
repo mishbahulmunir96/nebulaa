406e0cd5ad803315d8dc9813116af166a30fd584
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-11-14 22:52:52 +0700

    fix fetching data in team section about page

--- src/components/about/TeamSection.tsx
+++ src/components/about/TeamSection.tsx
@@ -11,16 +11,55 @@ interface TeamMember {
   position: string;
 }
 
+const positions = [
+  "Co-Founder",
+  "CEO",
+  "CTO",
+  "Manager",
+  "Kurir 1",
+  "Kurir 2",
+  // Tambahkan posisi lain jika perlu
+];
+
+const fetchTeamMembers = async () => {
+  try {
+    const response = await fetch("https://randomuser.me/api/?results=6");
+    const data = await response.json();
+
+    const members = data.results.map((member: any, index: number) => ({
+      src: member.picture.large,
+      alt: `${member.name.first} ${member.name.last}`,
+      name: `${member.name.first} ${member.name.last}`,
+      position: positions[index % positions.length],
+    }));
+
+    const dataToStore = {
+      members,
+      timeStamp: Date.now(),
+    };
+
+    localStorage.setItem("teamMembers", JSON.stringify(dataToStore));
+    return members; // Mengembalikan anggota tim
+  } catch (error) {
+    console.error(error);
+    return []; // Mengembalikan array kosong jika terjadi kesalahan
+  }
+};
+
 const TeamSection = () => {
   const [teamMembers, setTeamMembers] = useState<TeamMember[]>([]);
 
   useEffect(() => {
     const storedData = localStorage.getItem("teamMembers");
     if (storedData) {
-      const { members } = JSON.parse(storedData);
+      const { members }: { members: TeamMember[] } = JSON.parse(storedData);
       setTeamMembers(members);
+    } else {
+      fetchTeamMembers().then((fetchedMembers) => {
+        setTeamMembers(fetchedMembers);
+      });
     }
-  });
+  }, []);
 
   return (
     <Container
