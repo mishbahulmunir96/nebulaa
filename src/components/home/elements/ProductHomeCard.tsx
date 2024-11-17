66fad21584943a6c79249839054f32d821fb2457
mishbahulmunir96 <misbahulmunir13@outlook.co.id>
2024-11-17 09:18:19 +0700

    fix img product section homepage

--- src/components/home/elements/ProductHomeCard.tsx
+++ src/components/home/elements/ProductHomeCard.tsx
@@ -1,4 +1,3 @@
-import Box from "@/components/Box";
 import Image from "next/image";
 import { FC } from "react";
 
@@ -15,7 +14,7 @@ const ProductHomeCard: FC<ProductHomeCardProps> = ({
 }) => {
   return (
     <div className="relative mb-6 flex w-full flex-col overflow-hidden rounded-lg border bg-white shadow-md md:h-[225px] md:flex-row">
-      <div className="relative z-30 mx-3 mt-3 h-[250px] w-[375px] md:mx-auto md:my-auto md:ml-3 md:h-[200px] md:w-[300px]">
+      <div className="relative z-30 mx-auto mt-3 h-[250px] w-[330px] md:mx-auto md:my-auto md:ml-3 md:h-[200px] md:w-[300px]">
         <Image
           src={src}
           alt={title}
